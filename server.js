660ffa299c55335982d178fbc9f98677eb725df4
James Boom Boom Bamptini <33868129+bamptini@users.noreply.github.com>
2021-05-08 09:11:11 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,95 @@
+// Setup empty JS object to act as endpoint for all routes
+journalData = {};
+
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+app.use(express.urlencoded({ extended: false })); // Added from line 15
+app.use(express.json()); //Addded from line 16
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3002;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log ("Server Running");
+    console.log (`running on localhost: ${port}`);
+}
+
+//-------------- ROUTES ------------------//
+//
+//------- GET route Returns journalData object
+
+app.get('/all', getData); //Get feeling data when index URI is used
+
+function getData (request, response){
+    response.send(journalData); //send response to endpoint (object)
+    console.log(journalData);
+};
+
+//
+//--------POST ROUTE-------------------------
+
+app.post('/all', callBack); //URL needs to be defined #IMPORTANT URL CHANGE
+
+function callBack (request, response) {
+
+    let data = request.body;
+
+console.log('POST Update to server ', data);
+
+journalData["dttm"] = data.dttm;
+journalData["temp"] = data.temp;
+journalData["feeling"] = data.feeling;
+journalData["city"] = data.location;
+
+response.send(journalData);
+
+  //data.push();
+};
+
+//-------END POST ROUTE---------------------
+
+//------- POST JOURNAL to add incoming data to projectData endpoint - User input or API returns data, this data is stored as JSON in an Array
+
+/*const data = []; //Create data array to hold data
+
+app.post('/all', updateJournal); //postReceived callback function
+
+function updateJournal (request, response){
+    //response.send('Post Pushed')
+   // journalData.push(request.body);
+    
+// Add parts below into the POST JOURNAL
+
+let data = request.body;
+
+console.log('POST Update to server ', data);
+
+journalData["dttm"] = data.date;
+journalData["temp"] = data.temp;
+journalData["feeling"] = data.feeling;
+journalData["city"] = data.location;
+
+response.send(journalData);
+}*/
+
+
+
+
+
---
+++ website/app.js
@@ -0,0 +1,109 @@
+/* Global Variables */
+
+const baseUrl =  'http://api.openweathermap.org/data/2.5/weather?q=';
+const units = '&units=metric'; // convert f degree to C degree, append to end of key
+const apiKey = '&appid=1e32a5f263864d02667d1379ca8e179a';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+console.log('New date is '+ newDate);
+
+// variable to represent the City entered into UI
+const newCity = document.getElementById('city').value;
+console.log('New city is '+ newCity)  
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+//
+function performAction(e){
+  console.log('1')
+
+    // Get the value input into element to include in the POST
+    const feeling = document.getElementById('feelings').value; 
+    
+    // Call API to get weather data for city - based on user input into element.
+    newInput(baseUrl,newCity,apiKey,units)
+        
+    .then(function(data){
+      console.log('4')
+    // Add all data into POST request    
+        postData('/all', {
+
+                temp:data.main.temp, 
+                dttm:newDate,
+                location:newCity,
+                feeling:feeling});
+                console.log('6')
+            
+    }) .then( () =>{
+      console.log('7')
+        postUpdates()
+     }) 
+    };
+
+//GET data from WEB API using ASYNC
+    const newInput = async (baseURL, city, apiKey, units)=>{
+      console.log('2')  
+      console.log('New Input', {baseURL, city, apiKey, units})
+      const response = await fetch(baseURL+city+apiKey+units) // await until all data is received from API call, then try
+      try { // If fetch goes well    
+        const data = await response.json(); // Return data as JSON
+        console.log('3')
+        return data;
+      }  catch(error) { //If fetch goes wrong then error.
+        console.log("error", error);
+        // appropriately handle the error
+      }
+    }
+
+//POST DATA function
+const postData = async ( baseUrl = '', data = {})=>{
+  console.log('5')
+      const response = await fetch(baseUrl, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+    //console.log(response);
+
+      try {
+        const newData = await response.json();
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+  /* Function to GET Project Data 
+const getData = async (url='') =>{
+    console.log('Some URL ' +url);
+    const request = await fetch(url);
+    try {
+        const getData = await request.json()
+    }
+    catch(error){
+        console.log('Error', error);
+    }
+};*/
+
+  //CODE TO UPDATE UI
+
+  const postUpdates = async()=>{
+    
+        const entries = await fetch('/all');//baseUrl+city+apiKey+'/getData');
+        console.log('8')
+        try{
+            const projectData = await entries.json();
+            document.getElementById('date').innerHTML = `Date: ${projectData.dttm}`;
+            document.getElementById('temp').innerHTML = `The temperature is currently: ${projectData.temp} in ${projectData.location}`;
+            document.getElementById('content').innerHTML = `I am feeling: ${projectData.feeling}`;
+        }
+        catch(err){
+            console.log('Error posting data ' + err);
+
+        }}
