284d83fe2f6693f82f76ea1acb678468488ca21f
Bampton, James (jb704y) <jb704y@intl.att.com>
2021-08-12 09:32:24 +0100

    updated newInpuitGEO to post both sets of data. Plus other updates

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,5 +1,4 @@
-// CREATE VARIABLE FOR HIDING API KEY - These variables come from server.js, is this correct ????
-
+// CREATE VARIABLES FOR HIDING API KEYS
 // GEO API REFERENCES
 let GEO_baseUrl = "http://api.geonames.org/searchJSON?q=";
 const GeoAPIUser = process.env.GEO_API_USERNAME;
@@ -12,26 +11,19 @@ const WEATHER_API_KEY = process.env.WEATHERBIT_API_KEY;
 
 // PIXABAY API REFERENCES
 const PIXABAY_API_KEY = process.env.PIXABAT_API_KEY;
-const PIX_baseUrl = "https://pixabay.com/api/?key=" + PIXABAY_API_KEY + "&q="; //+ city + '&image_type=photo&category=places' ;
-//const GeoapiKey = process.env.PIXABAY_API_ID;
+const PIX_baseUrl = "https://pixabay.com/api/?key=" + PIXABAY_API_KEY + "&q="; 
 //Example - https://pixabay.com/api/?key={ KEY }&q=yellow+flowers&image_type=photo
 //Example - https://pixabay.com/api/?key={####}&q=Portsmouth&image_type=photo&category=places
 
 console.log("PIX URL = " + PIX_baseUrl);
 
-/* Global Variables Possibly not needed as they are defined in server.js */
-//const baseUrlGEO = 'http://api.geonames.org/searchJSON?q=';
-//const city = '';
+//Global Variables 
 const userName = "&username=" + process.env.GEO_API_USERNAME;
 
 // Create a new date instance dynamically with JS
 let date = new Date();
 let newDate = date.getDate() + "." + date.getMonth() + "." + date.getFullYear();
 console.log("New date is " + newDate);
-
-/* //Add event listener for submit button
-document.getElementById("getButton").addEventListener('click', performAction);
-console.log('Just added event listener to submit button - application.js') */
 
 export async function performAction(e) {
   console.log("1 - Perform Action function");
@@ -42,8 +34,6 @@ export async function performAction(e) {
 
   const startDate = document.getElementById("date").value;
   console.log("Date is " + date);
-  //console.log("BaseURL is : " + GEO_baseUrl);
-  //console.log("Full URL is : " + GEO_baseUrl + city + userName);
 
   let body;
   if (city != "") {
@@ -53,49 +43,61 @@ export async function performAction(e) {
 
     //Call newInputGEO function passing in the API call, using the URL, city entered by user and username 
     newInputGEO(GEO_baseUrl, city, userName)
-      //data is retunred from newInputGEO then...
+  
+    //data is retunred from newInputGEO then...
       .then(function (result) {
         console.log("4");
-        console.log(result);
+        //console.log(result);
 
     //Create new variable from results for using in Weather App
-        const longitude = result.lng
-        const latitude = result.lat
-        console.log(longitude)
-        console.log(latitude)
-        const weatherString="lat="+latitude+"&lon="+longitude+"&key="
-        console.log(weatherString)
-        
-    //Call postData function passing in five values of city, longitude, latitude, population and country
-        postData("/location", {
+          const longitude = result.lng
+          const latitude = result.lat
+          console.log(longitude)
+          console.log(latitude)
+          const weatherString="lat="+latitude+"&lon="+longitude+"&key="
+          console.log(weatherString)
+
+          newInputWeather(WEATHER_baseUrl, weatherString, WEATHER_API_KEY).then(function (weather) {
+              console.log("7");
+              postData("/location", {
+                city: result.name,
+                longitude: result.lng,
+                latitude: result.lat,
+                population: Math.round(result.population/1000000).toFixed(2)+'m',
+                country: result.countryName,
+                    temperature: weather.temp,
+                    low: weather.low_temp,
+                    high: weather.high_templat,
+                    sunrise: weather.sunrise_ts,
+
+                  });
+                  console.log("9");
+           });
+
+        /*newInputWeather(WEATHER_baseUrl, weatherString, WEATHER_API_KEY)
+          .then(function (wResult) {
+          console.log("10")
+          //console.log(wResult)
+
+        console.log("weather API called")
+
+        postData("/weather", {
           city: result.name,
           longitude: result.lng,
           latitude: result.lat,
           population: Math.round(result.population/1000000).toFixed(2)+'m',
           country: result.countryName,
-        });
-        
-        console.log("6");
-      })
-    
-  }
-      newInputWeather(WEATHER_baseUrl, weatherString, WEATHER_API_KEY) 
-      .then(function (weather) {
-
-        console.log("weather API called");
-
-        postData("/location", {
-          temperature: weather.temp,
-          low: weather.low_temp,
-          high: weather.high_templat,
-          sunrise: weather.sunrise_ts,
+          temperature: wResult.temp,
+          low: wResult.low_temp,
+          high: wResult.high_templat,
+          sunrise: wResult.sunrise_ts,
         });
         console.log("7");
 ///////////////////////////////////////////////////TEMP REMOVAL //       postUpdates(); // Add all data from into POST request
-  });
-  // Call API to get geonames details for specific place - based on user input into element.
-
-  // TODO create three async fucntion to get data form eahc of the API calls
+  });*/
+      })   
+  }
+    
   await fetch("/postTripData", {
     method: "POST",
     credentials: "same-origin",
@@ -137,7 +139,7 @@ const newInputGEO = async (GEO_baseUrl, city, userName) => {
     const data = await response.json(); // Return data as JSON
     console.log("3");
     let result = data.geonames[0];
-    console.log(result);
+    //console.log(result);
     return result;
    // return data;
   } catch (error) {
@@ -150,15 +152,14 @@ const newInputGEO = async (GEO_baseUrl, city, userName) => {
 
 //GET data from WEATHER API using ASYNC function. Results will be for the City entered by user
 const newInputWeather = async (WEATHER_baseUrl, weatherString, WEATHER_API_KEY) => {
-  console.log("2");
-  //console.log("New Input", { GEO_baseUrl, city, userName });
+  console.log("5")
   const wResponse = await fetch(WEATHER_baseUrl + weatherString + WEATHER_API_KEY); // await until all data is received from API call, then try
   try {
-    // If fetch works, convert 'response' into json and store in 'data'
+    // If fetch works, convert 'response' into json and store in 'weather'
     const weather = await wResponse.json(); // Return data as JSON
-    console.log("3");
-    let wResult = weather.data[0];
-    console.log(wResult);
+    const wResult = weather.data[0];
+    //console.log(wResult);
+    console.log("6")
     return wResult;
    // return data;
   } catch (error) {
@@ -177,7 +178,7 @@ const newInputPIX = async (PIX_baseUrl, city, userName) => {
   try {
     // If fetch works, convert 'response' into json and store in 'data'
     const data = await response.json(); // Return data as JSON
-    console.log("3");
+    console.log("newInputPIX -");
     let result = data.geonames[0];
     console.log(result);
     return result;
@@ -189,9 +190,10 @@ const newInputPIX = async (PIX_baseUrl, city, userName) => {
   }
 };
 
-//POST DATA function
-const postData = async (Url = "", data = {}) => {
-  console.log("5");
+//POST DATA function -- ADDED export key word as a prefix to const postData
+export const postData = async (Url = "", data = {}) => {
+  console.log("8 postDataCalled");
+  console.log(Url)
   console.log(data)
   const response = await fetch(Url, {
     method: "POST",
@@ -202,13 +204,15 @@ const postData = async (Url = "", data = {}) => {
     // Body data type must match "Content-Type" header
     body: JSON.stringify(data),
   });
-  //console.log(response);
+  console.log("postData ended");
   try {
     const newData = await response.json();
+    console.log("New data = " + newData)
     return newData;
   } catch (error) {
-    console.log("error", error);
+    console.log("These is an error in postData function:", error);
   }
+  console.log("Returning newData to ")
 };
 
 //CODE TO UPDATE UI
