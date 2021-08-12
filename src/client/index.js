4494c279a2482042a858f41883126b16f24a7fa9
Bampton, James (jb704y) <jb704y@intl.att.com>
2021-08-12 09:35:30 +0100

    Missed these commits form previous

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,21 @@
-import './styles/all-styles.scss'
-import logo from './media/blue-globe.jpg'
-import "./js/application"
-import { performAction } from './js/application'
-import { postData } from './js/postData'
+import "./styles/all-styles.scss";
+import logo from "./media/blue-globe.jpg";
+import "./js/application";
+import { performAction } from "./js/application";
+//import {postData} from "./js/postData";
+//import { postData } from "./js/postData";
+//import { postData } from "./js/application";
 //import { handleSubmit } from './js/formHandler'
 
-document.getElementById('logo2').src = logo
+document.getElementById("logo2").src = logo;
 //Images defined to use inside cards but not currenlty being used.
 //document.getElementById('card1logo').src = img
 //document.getElementById('card2logo').src = img
 
 // Add event listener for submit button
-document.getElementById("getButton").addEventListener('click', performAction);
-console.log('Just added event listener to submit button in index.js - probably can be removed')
+document.getElementById("getButton").addEventListener("click", performAction);
+console.log(
+  "Event listener addded"
+);
 
-export{performAction, postData}
+//export { performAction, postData }; // |Took this out and also line5 & 6 above 
--- src/server/server.js
+++ src/server/server.js
@@ -3,30 +3,11 @@
 endPoint = {};
 
 // CREATE VARIABLE FOR HIDING API KEY
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-/* GEO API REFERENCES
-let GEO_baseUrl = 'http://api.geonames.org/searchJSON?q=';
-const GeoAPIUser = process.env.GEO_API_USERNAME;
-//Example - http://api.geonames.org/searchJSON?q=London&username=GeoAPIUser
-
-// WEATHER API REFERENCES
-const WEATHER_baseUrl = 'http://api.weatherbit.io/v2.0/forecast/daily?';
-const WEATHER_API_KEY = process.env.WEATHERBIT_API_KEY;
-//Example - http://api.weatherbit.io/v2.0/forecast/daily?lat=35.555&lon=-45.333&key=xxxxxxxxxx
-
-// PIXABAY API REFERENCES
-const PIXABAY_API_KEY = process.env.PIXABAT_API_KEY;
-const PIX_baseUrl = 'https://pixabay.com/api/?key=' + PIXABAY_API_KEY + '&q='; //+ city + '&image_type=photo&category=places' ;
-//const GeoapiKey = process.env.PIXABAY_API_ID;
-//Example - https://pixabay.com/api/?key={ KEY }&q=yellow+flowers&image_type=photo
-//Example - https://pixabay.com/api/?key={####}&q=Portsmouth&image_type=photo&category=places
-
-console.log('PIX URL = ' + PIX_baseUrl);*/
-
 // START EXPRESS
-const express = require('express')
+const express = require("express");
 
 // START APP
 const app = express();
@@ -36,55 +17,64 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // OTHER DEPENDENCIES
-const path = require('path')
-const bodyParser = require('body-parser');
-app.use(bodyParser.json())
+const path = require("path");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({
-    extended: true
-  }))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 // CORS FOR CROSS ORIGIN ALLOWANCE
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-// SETUP DEPLOYMENT FOLDER 
-app.use(express.static('dist'))
+// SETUP DEPLOYMENT FOLDER
+app.use(express.static("dist"));
 console.log(__dirname);
 
 // ASSIGN PORT
 app.listen(8086, function () {
-    console.log('Example app listening on production port 8086!')
+  console.log("Example app listening on production port 8086!");
 });
 
 //-------------- ROUTES ------------------//
 
 //------- GET route Returns locationData object
-app.get('/location', getLocation); //Get data for entered Location
+app.get("/location", getLocation); //Get data for entered Location
 
-function getLocation (request, response){
-    response.send(endPoint); //send response to endpoint (object)
-    console.log(endPoint);
-};
+function getLocation(request, response) {
+  response.send(endPoint); //send response to endpoint (object)
+  console.log(endPoint);
+}
+
+//------- GET route Returns locationData object
+app.get("/weather", getWeather); //Get data for entered Location
+
+function getWeather(request, response) {
+  response.send(endPoint); //send response to endpoint (object)
+  console.log("sending to endpoint " + endPoint);
+}
 
 // NEW POST CODE - Maybe re use below
+app.post("/postTripData", async (request, response) => {
+  let destination = req.body.city;
+  console.log("Destination is " + destination);
 
-app.post('/postTripData', async (request, response) =>
-{
-    let destination = req.body.city;
-    console.log('Destination is '+ destination);
+  let holdData = get(
+    GEO_baseUrl + destination + "&maxRows=1&username=" + GEO_API_USERNAME
+  );
+  console.log("holdData is set to: " + holdData);
+  await holdData.then(async (response) => {
+    let lat = holdData[0].lng;
+    let long = holdData[0].lat;
 
-    let  holdData = get(GEO_baseUrl + destination + "&maxRows=1&username=" + GEO_API_USERNAME);
-    console.log ("holdData is set to: " + holdData);
-    await holdData.then (async response =>{
-
-        let lat = holdData[0].lng;
-        let long = holdData[0].lat;
-
-        let weather = get(WEATHER_baseUrl + 'lat=' + lat +'&lon=' + long);
-        console.log (weather);
-        let image = get(PIX_baseUrl + destination + "&image_type=photo");
-    });
+    let weather = get(WEATHER_baseUrl + "lat=" + lat + "&lon=" + long);
+    console.log(weather);
+    let image = get(PIX_baseUrl + destination + "&image_type=photo");
+  });
 });
 
 /*--------POST ROUTE-------------------------
@@ -105,6 +95,5 @@ endPoint["city"] = data.location;
 
 response.send(endPoint);
 };*/
-
 
 //-------END POST ROUTE---------------------
